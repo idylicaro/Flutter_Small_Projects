a27512d4964c6e192dda35627a463ae3589389e1
Idyl Icaro dos Santos <idylicaro.se@hotmail.com>
2021-06-29 16:03:19 -0300

    feat: create user profile screen

--- login_system/lib/pages/profile_page.dart
+++ login_system/lib/pages/profile_page.dart
@@ -2,6 +2,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:google_sign_in/google_sign_in.dart';
+import 'package:getwidget/getwidget.dart';
 
 class UserInfoCard extends StatefulWidget {
   final User user;
@@ -17,119 +18,59 @@ class UserInfoCardState extends State<UserInfoCard> {
   Widget build(BuildContext context) {
     return Card(
       child: Padding(
-        padding: const EdgeInsets.all(16),
+        padding: const EdgeInsets.all(30),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Container(
-              padding: const EdgeInsets.only(bottom: 8),
-              alignment: Alignment.center,
-              child: const Text(
-                'User info',
-                style: TextStyle(fontWeight: FontWeight.bold),
-              ),
+            Center(
+              child: GFAvatar(
+              backgroundImage: NetworkImage(widget.user.photoURL),
+              radius: 60,
             ),
-            if (widget.user != null)
-              if (widget.user.photoURL != null)
-                Container(
-                  alignment: Alignment.center,
-                  margin: const EdgeInsets.only(bottom: 8),
-                  child: Image.network(widget.user.photoURL),
-                )
-              else
-                Align(
-                  child: Container(
-                    padding: const EdgeInsets.all(8),
-                    margin: const EdgeInsets.only(bottom: 8),
-                    color: Colors.black,
-                    child: const Text(
-                      'No image',
-                      textAlign: TextAlign.center,
-                    ),
-                  ),
+            ),
+            Center(
+              child: GFCard(
+                boxFit: BoxFit.cover,
+                title: GFListTile(
+                  titleText: widget.user.displayName,
+                  subtitleText: widget.user.email,
                 ),
-            Text(widget.user == null
-                ? 'Not signed in'
-                : '${widget.user.isAnonymous ? 'User is anonymous\n\n' : ''}'
-                    'Email: ${widget.user.email} (verified: ${widget.user.emailVerified})\n\n'
-                    'Phone number: ${widget.user.phoneNumber}\n\n'
-                    'Name: ${widget.user.displayName}\n\n\n'
-                    'ID: ${widget.user.uid}\n\n'
-                    'Tenant ID: ${widget.user.tenantId}\n\n'
-                    'Refresh token: ${widget.user.refreshToken}\n\n\n'
-                    'Created: ${widget.user.metadata.creationTime.toString()}\n\n'
-                    'Last login: ${widget.user.metadata.lastSignInTime}\n\n'),
-            if (widget.user != null)
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.stretch,
-                children: [
-                  Text(
-                    widget.user.providerData.isEmpty
-                        ? 'No providers'
-                        : 'Providers:',
-                    style: const TextStyle(fontWeight: FontWeight.bold),
-                    textAlign: TextAlign.center,
-                  ),
-                  for (var provider in widget.user.providerData)
-                    Dismissible(
-                      key: Key(provider.uid),
-                      onDismissed: (action) =>
-                          widget.user.unlink(provider.providerId),
-                      child: Card(
-                        color: Colors.grey[700],
-                        child: ListTile(
-                          leading: provider.photoURL == null
-                              ? IconButton(
-                                  icon: const Icon(Icons.remove),
-                                  onPressed: () =>
-                                      widget.user.unlink(provider.providerId))
-                              : Image.network(provider.photoURL),
-                          title: Text(provider.providerId),
-                          subtitle: Text(
-                              "${provider.uid == null ? "" : "ID: ${provider.uid}\n"}"
-                              "${provider.email == null ? "" : "Email: ${provider.email}\n"}"
-                              "${provider.phoneNumber == null ? "" : "Phone number: ${provider.phoneNumber}\n"}"
-                              "${provider.displayName == null ? "" : "Name: ${provider.displayName}\n"}"),
-                        ),
-                      ),
-                    ),
-                ],
-              ),
-            Visibility(
-              visible: widget.user != null,
-              child: Container(
-                margin: const EdgeInsets.only(top: 8),
-                alignment: Alignment.center,
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                color: Colors.white,
+                content: Column(
                   children: [
-                    IconButton(
-                      onPressed: () => widget.user.reload(),
-                      icon: const Icon(Icons.refresh),
+                    GFTypography(
+                      text: widget.user.uid,
+                      type: GFTypographyType.typo6,
+                      icon: Icon(Icons.perm_identity, color: Colors.greenAccent),
                     ),
-                    IconButton(
-                      onPressed: () => showDialog(
-                        context: context,
-                        builder: (context) => UpdateUserDialog(widget.user),
-                      ),
-                      icon: const Icon(Icons.text_snippet),
+                    Divider(),
+                    GFTypography(
+                      text: widget.user.emailVerified ? 'Email Verified': 'email not verified',
+                      type: GFTypographyType.typo5,
+                      icon: widget.user.emailVerified != null ? Icon(Icons.check, color: Colors.green) : Icon(Icons.block, color: Colors.redAccent),
                     ),
-                    IconButton(
-                      onPressed: () => widget.user.delete(),
-                      icon: const Icon(Icons.delete_forever),
+                    Divider(),
+                    GFTypography(
+                      text: widget.user.phoneNumber != null ? widget.user.phoneNumber: 'No cell number',
+                      type: GFTypographyType.typo5,
+                      icon: widget.user.phoneNumber != null ? Icon(Icons.check, color: Colors.green) : Icon(Icons.block, color: Colors.redAccent),
                     ),
                   ],
                 ),
               ),
             ),
             Center(
-                child:ElevatedButton(
-                  onPressed: () async {
-                    await singOut(context);
+              child: GFButton(
+                onPressed: () async {
+                  await singOut(context);
+                },
+                text: 'SingOut',
+                color: GFColors.DANGER,
+                icon: Icon(Icons.exit_to_app),
+                position: GFPosition.end,
+                size: GFSize.LARGE,
 
-                  },
-                  child: const Text('SingOut'),
-                ),
+              ),
             ),
           ],
         ),
@@ -138,82 +79,8 @@ class UserInfoCardState extends State<UserInfoCard> {
   }
 }
 
-Future<void> singOut(context) async{
+Future<void> singOut(context) async {
   await GoogleSignIn().disconnect();
   await FirebaseAuth.instance.signOut();
   Navigator.pop(context);
-}
-
-class UpdateUserDialog extends StatefulWidget {
-  final User user;
-
-  const UpdateUserDialog(this.user);
-
-  @override
-  _UpdateUserDialogState createState() => _UpdateUserDialogState();
-}
-
-class _UpdateUserDialogState extends State<UpdateUserDialog> {
-  TextEditingController _nameController;
-  TextEditingController _urlController;
-
-  @override
-  void initState() {
-    _nameController = TextEditingController(text: widget.user.displayName);
-    _urlController = TextEditingController(text: widget.user.photoURL);
-    super.initState();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return AlertDialog(
-      title: const Text('Update profile'),
-      content: SingleChildScrollView(
-        child: ListBody(
-          children: [
-            TextFormField(
-              controller: _nameController,
-              autocorrect: false,
-              decoration: const InputDecoration(labelText: 'displayName'),
-            ),
-            TextFormField(
-              controller: _urlController,
-              decoration: const InputDecoration(labelText: 'photoURL'),
-              autovalidateMode: AutovalidateMode.onUserInteraction,
-              autocorrect: false,
-              validator: (String value) {
-                if (value.isNotEmpty) {
-                  var uri = Uri.parse(value);
-                  if (uri.isAbsolute) {
-                    //You can get the data with dart:io or http and check it here
-                    return null;
-                  }
-                  return 'Faulty URL!';
-                }
-                return null;
-              },
-            ),
-          ],
-        ),
-      ),
-      actions: [
-        TextButton(
-          onPressed: () {
-            widget.user.updateProfile(
-                displayName: _nameController.text,
-                photoURL: _urlController.text);
-            Navigator.of(context).pop();
-          },
-          child: const Text('Update'),
-        )
-      ],
-    );
-  }
-
-  @override
-  void dispose() {
-    _nameController.dispose();
-    _urlController.dispose();
-    super.dispose();
-  }
 }
